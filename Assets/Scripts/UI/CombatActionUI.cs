522b550d9cb3dd71ab8877b46f385fa2b98b62c5
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2022-06-07 23:03:08 -0500

    I made the theme of Combat Actions, that is, put the powers that the player and enemy will have in the RPG combat

---
+++ Assets/Scripts/UI/CombatActionUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CombatActionUI : MonoBehaviour
+{
+    public GameObject panel;
+    public CombatActionButton[] buttons;
+    public GameObject descriptionPanel;
+    public TextMeshProUGUI descriptionText;
+
+}
