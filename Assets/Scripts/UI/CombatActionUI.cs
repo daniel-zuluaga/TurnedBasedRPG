25bc3bf2d1b8b700c712e109e050afed341c3e29
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2022-06-08 22:01:20 -0500

    What I did is finish the fighting part and add things like melee, heal, effects and ranged

--- Assets/Scripts/UI/CombatActionUI.cs
+++ Assets/Scripts/UI/CombatActionUI.cs
@@ -10,4 +10,65 @@ public class CombatActionUI : MonoBehaviour
     public GameObject descriptionPanel;
     public TextMeshProUGUI descriptionText;
 
+    void OnEnable()
+    {
+        TurnManager.instance.onNewTurn += OnNewTurn;
+    }
+
+    void OnDisable()
+    {
+        TurnManager.instance.onNewTurn -= OnNewTurn;
+
+    }
+
+    void OnNewTurn()
+    {
+        if (TurnManager.instance.GetCurrentTurnCharacter().team == Character.Team.Player)
+        {
+            DisplayCombatActions(TurnManager.instance.GetCurrentTurnCharacter());
+        }
+        else
+        {
+            DisableCombatActions();
+        }
+
+    }
+
+    public void DisplayCombatActions(Character character)
+    {
+        panel.SetActive(true);
+
+        for(int i = 0; i < buttons.Length; i++)
+        {
+            if (i < character.combatActions.Length)
+            {
+                buttons[i].gameObject.SetActive(true);
+                buttons[i].SetCombatAction(character.combatActions[i]);
+            }
+            else
+            {
+                buttons[i].gameObject.SetActive(false);
+            }
+        }
+
+    }
+
+    public void DisableCombatActions()
+    {
+        panel.SetActive(true);
+        DisableCombatActionDescription();
+
+    }
+
+    public void SetCombatActionDescription(CombatAction combatAction)
+    {
+        descriptionPanel.SetActive(true);
+        descriptionText.text = combatAction.description;
+    }
+
+    public void DisableCombatActionDescription()
+    {
+        descriptionPanel.SetActive(false);
+    }
+
 }
